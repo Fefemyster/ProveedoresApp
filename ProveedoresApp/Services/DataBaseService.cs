cc2f12125cbcea511a5e345a869748d2bcee1845
Fefemyster <kenennlara@gmail.com>
2025-09-23 20:32:34 -0600

    Update

---
+++ ProveedoresApp/Services/DataBaseService.cs
@@ -0,0 +1,40 @@
+﻿
+using ProveedoresApp.Models;
+using SQLite;
+
+namespace ProveedoresApp.Services
+{
+    public class DataBaseService : IDataBaseService
+    {
+        private SQLiteAsyncConnection _db;
+
+        public DataBaseService() 
+        {
+            if (_db != null) return;
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Productos.db3");
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Proveedor>();
+
+        }
+
+        public Task<int> CreateProveedor(Proveedor proveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteProveedor(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Proveedor>> GetAllProveedores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateProveedor(Proveedor proveedor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ProveedoresApp/Services/IDataBaseService.cs
+++ ProveedoresApp/Services/IDataBaseService.cs
@@ -4,7 +4,11 @@ namespace ProveedoresApp.Services
 {
     public interface IDataBaseService
     {
-        Task<List<Proveedor>> GetAllProveedores();
-            
+
+        Task<List<Proveedor>> GetAllProveedores(); //Obtener todos los proveedores
+        Task<int> CreateProveedor (Proveedor proveedor); //Crear un nuevo proveedor
+        Task<int> UpdateProveedor (Proveedor proveedor); //Actualizar un proveedor existente
+        Task<int> DeleteProveedor (int id); //Eliminar un proveedor por su Id
+
     }
 }
